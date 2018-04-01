dfbc4b4866c81802e055aba1949de071a40f9ac9
Kayla Cross <kac288@georgetown.edu>
2018-04-01 18:28:03 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -16,7 +16,9 @@ class Scraper
   end
 
   def get_page
-    page = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    @page = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
   end
+
+  def get_courses
 
 end
