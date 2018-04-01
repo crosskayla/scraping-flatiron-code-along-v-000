cb76971cb1a17271677682d75fd866f39ffb1f68
Kayla Cross <kac288@georgetown.edu>
2018-04-01 18:26:40 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,8 +4,7 @@ require 'open-uri'
 require_relative './course.rb'
 
 class Scraper
-  
-    def print_courses
+  def print_courses
     self.make_courses
     Course.all.each do |course|
       if course.title
@@ -15,8 +14,5 @@ class Scraper
       end
     end
   end
-  
+
 end
-
-
-
